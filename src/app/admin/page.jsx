13db109a56110521c0ae815a966074cdeb7e7f3c
David Perelman <dperelman3@gmail.com>
2024-08-27 19:10:21 +0300

    use cloudinary video

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,9 +1,16 @@
+"use client";
+
+import { upload } from "@/lib/upload";
 import styles from "./admin.module.css";
-import { handleGithubLogin } from "@/lib/action";
+// import { handleGithubLogin } from "@/lib/action";
 // import RegisterForm from "@/components/registerForm/RegisterForm";
 // import LoginForm from "@/components/loginForm/LoginForm";
+import { useFormState } from "react-dom";
 
-const AdminPage = async () => {
+const AdminPage = () => {
+  const [url, formAction] = useFormState(upload, null);
+  console.log(url);
+
   return (
     <div className={styles.container}>
       <h1>ניהול אתר</h1>
@@ -13,8 +20,17 @@ const AdminPage = async () => {
           <br />
           {/* <LoginForm /> */}
           <br />
-          <form action={handleGithubLogin}>
+          {/* <form action={handleGithubLogin}>
             <button>Login with github</button>
+          </form> */}
+          <form action={formAction}>
+            <input type="file" accept="video/*" name="video" />
+            <button
+              type="submit"
+              className="bg-blue-800 text-white p-2 rounded-md"
+            >
+              Upload
+            </button>
           </form>
         </div>
       </div>
--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -5,13 +5,13 @@ const Video = () => {
   return (
     <main>
       {/* <video
-        src={require("https://vod.api.video/vod/vi67FW5vvj367CiuS5EwK1zd/mp4/source.mp4")}
+        src={require("https://videos.pexels.com/video-files/27591301/12177940_2560_1440_60fps.mp4")}
         autoPlay
         muted
         loop
         className={styles.container}
       /> */}
-      <BackgroundVideo src="https://vod.api.video/vod/vi67FW5vvj367CiuS5EwK1zd/mp4/source.mp4"></BackgroundVideo>
+      <BackgroundVideo src="https://res.cloudinary.com/dyzl8mvwt/video/upload/f_auto:video,q_auto/my_video"></BackgroundVideo>
     </main>
   );
 };
