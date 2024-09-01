3328e543ff838c1c3ab1a1cf9deba04296a2bbcc
David Perelman <dperelman3@gmail.com>
2024-09-01 20:58:59 +0300

    SingleProjectContainer project description data UI

--- src/components/singleProjectContainer/SingleProjectContainer.jsx
+++ src/components/singleProjectContainer/SingleProjectContainer.jsx
@@ -3,14 +3,26 @@
 import Image from "next/image";
 import styles from "./singleProjectContainer.module.css";
 import ImagesCarousel from "../imagesCarousel/ImagesCarousel";
+import Link from "next/link";
 
 const SingleProjectContainer = ({ project }) => {
-  console.log(project);
-
   return (
     <div className={styles.content}>
       <div className={styles.leftCol}>
-        <div className={`${styles.leftPanel} ${styles.panel}`}></div>
+        <div className={`${styles.leftPanel} ${styles.panel}`}>
+          <h1>{project.description}</h1>
+          <h3>
+            {project.price} {String.fromCharCode(0x20aa)}
+          </h3>
+          <Link href="/" className={styles.links}>
+            <h4>עציצים</h4>
+          </Link>
+          <Link href="/" className={styles.links}>
+            <h4>עץ</h4>
+          </Link>
+          <p>{project.description}</p>
+          <h3 className={styles.size_header}>{project.size}</h3>
+        </div>
       </div>
       <div className={styles.rightCol}>
         <div className={`${styles.rightPanel} ${styles.panel}`}>
--- src/lib/models.js
+++ src/lib/models.js
@@ -16,9 +16,39 @@ const projectSchema = new mongoose.Schema(
     title: { type: String, required: true },
     description: { type: String, required: true },
     date: { type: String },
-    images: { type: [String] },
+    images: { type: [String], required: true },
     thumbnail: { type: String },
     slug: { type: String, required: true, unique: true },
+    price: { type: String, required: true },
+    size: { type: String, required: true },
+    division: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "Division",
+      required: true,
+    },
+    material_type: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "Material",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const divisionSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    projects: { type: [mongoose.Schema.Types.ObjectId], ref: "Project" },
+  },
+  { timestamps: true }
+);
+
+const materialSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    projects: { type: [mongoose.Schema.Types.ObjectId], ref: "Project" },
   },
   { timestamps: true }
 );
@@ -26,3 +56,7 @@ const projectSchema = new mongoose.Schema(
 export const User = mongoose.models.User || mongoose.model("User", userSchema);
 export const Project =
   mongoose.models.Project || mongoose.model("Project", projectSchema);
+export const Division =
+  mongoose.models.Division || mongoose.model("Division", divisionSchema);
+export const Material =
+  mongoose.models.Material || mongoose.model("Material", materialSchema);
