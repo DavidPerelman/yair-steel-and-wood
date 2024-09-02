915e36a105e96de0931cc7a9bb7c79380a023c37
David Perelman <dperelman3@gmail.com>
2024-09-02 20:43:53 +0300

    change contactLinks heigth

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,9 +1,25 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import styles from "./loading.module.css";
 import LoadingSpinner from "@/components/loadingSpinner/LoadingSpinner";
+import { useEffect } from "react";
 
 const Loading = () => {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const loading = document.querySelector("#loading");
+
+    if (pathname === "/") {
+      loading.classList.add(styles.home);
+    } else {
+      loading.classList.remove(styles.home);
+    }
+  }, []);
+
   return (
-    <div className={styles.loading}>
+    <div id="loading" className={styles.loading}>
       <LoadingSpinner />
     </div>
   );
