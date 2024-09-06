2f04644abd82ffb8e662047dcae915719948be72
David Perelman <dperelman3@gmail.com>
2024-09-06 16:07:50 +0300

    check logo bug

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -111,7 +111,7 @@ const Navbar = ({ open, setOpen }) => {
     <div id="navbar" className={styles.container}>
       <Links open={open} openMenuClick={openMenuClick} />
       <Link href="/">
-        <Image src="/logo.png" width={95} height={95} alt="logo" />
+        <Image src="/menu.svg" width={95} height={95} alt="logo" />
       </Link>
       <div className={styles.contactLinks}>
         <Link target="_blank" href="tel:0584422401">
