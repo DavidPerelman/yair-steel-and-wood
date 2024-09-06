267a84feca0b53a172999c7decff4dcdeab15993
David Perelman <dperelman3@gmail.com>
2024-09-06 16:15:14 +0300

    change logo src

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -111,7 +111,12 @@ const Navbar = ({ open, setOpen }) => {
     <div id="navbar" className={styles.container}>
       <Links open={open} openMenuClick={openMenuClick} />
       <Link href="/">
-        <Image src="/logo.svg" width={95} height={95} alt="logo" />
+        <Image
+          src="https://res.cloudinary.com/dflevhwgh/image/upload/v1725533005/yh0u2addb6vxjwdfhvnn.png"
+          width={95}
+          height={95}
+          alt="logo"
+        />
       </Link>
       <div className={styles.contactLinks}>
         <Link target="_blank" href="tel:0584422401">
