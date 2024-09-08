8e695b72449b60be79a7c643094515510280efa1
David Perelman <dperelman3@gmail.com>
2024-09-08 13:51:38 +0300

    about text example

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -10,6 +10,13 @@ const AboutPage = async () => {
   const data = await getPost("about");
   const postData = JSON.parse(JSON.stringify(data));
 
+  const text = [
+    "שלום וברוכים הבאים לאתר שלי. ",
+    "‏‎עבודה בעץ תמיד הייתה תחום שעניין אותי. הפרויקט הראשון היה הנדנדה של סבא שלי, שראיתי וניסיתי לשחזר בכוחות עצמי. עם הזמן גיליתי איך העולם הזה מושך לי את תשומת הלב והדמיון ובמהלך השנים לקחתי חלק בפרויקטים מסוגים שונים.",
+    "לתחום הברזל הגעתי רק אחרי השחרור מהצבא. עבודת הריתוך בחומרים קטנים היא מורכבת ועדינה ודורשת סבלנות, אז הקדשתי זמן בסדנה עם רתכת וערימה של אלקטרודות עד שהתקרבתי לאיכות הרצויה. כך, לאט לאט, הידע והכישורים שלי השתפרו, והתחלתי להשתמש בהם ליצירה.",
+    "מהר מאוד גיליתי את היופי שבשילוב בין ברזל ועץ, העוצמה והברק של הברזל השחור יחד עם החום והגוון של העץ החי פתחו בפניי עולם שלם של יצירה, פשוטה ומלאת דיוק. ",
+  ];
+
   return (
     <>
       <CustomHead
@@ -46,7 +53,40 @@ const AboutPage = async () => {
       )}
       {/* </div> */}
       {/* <PostContainer data={postData} /> */}
-      <FullScreenImage />
+      {/* <FullScreenImage /> */}
+      <>
+        <div
+          style={{
+            zIndex: -1,
+            position: "fixed",
+            width: "100vw",
+            height: "100vh",
+          }}
+        >
+          <Image
+            src="https://res.cloudinary.com/dflevhwgh/image/upload/v1725387382/ve44cvyyuq7acec8six2.jpg"
+            alt="Mountains with snow"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        <h1
+          style={{
+            paddingTop: "30vh",
+            // fontFamily: "monospace",
+            // fontSize: "3.5rem",
+            // fontWeight: "bold",
+            textAlign: "center",
+          }}
+        >
+          אודות
+        </h1>
+        <div className={styles.whiteFilter}>
+          {text.map((item, i) => (
+            <p key={i}>{item}</p>
+          ))}
+        </div>
+      </>
     </>
   );
 };
