b7ecb6bd1af7ac20490103931f714e57e852c99d
David Perelman <dperelman3@gmail.com>
2024-09-10 14:21:26 +0300

    filter projects by divisions

--- src/app/panel/projectsPanel/page.jsx
+++ src/app/panel/projectsPanel/page.jsx
@@ -4,8 +4,8 @@ import ProjectsContainer from "@/components/projectsContainer/ProjectsContainer"
 import { getProjects } from "@/lib/data";
 
 const AdminProjectsPage = async () => {
-  const data = await getProjects();
-  const projects = JSON.parse(JSON.stringify(data));
+  // const data = await getProjects();
+  // const projects = JSON.parse(JSON.stringify(data));
 
   return (
     <div className={styles.container}>
@@ -15,7 +15,7 @@ const AdminProjectsPage = async () => {
       />
       <h1 className={styles.pageHeader}>עריכת פרויקטים</h1>
       <div className={styles.pageHeader}></div>
-      <ProjectsContainer projects={projects} link="/panel/projectsPanel/" />
+      {/* <ProjectsContainer projects={projects} link="/panel/projectsPanel/" /> */}
     </div>
   );
 };
