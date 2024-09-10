b0970b406da363d7229cd556dd80a02b4cd155b7
David Perelman <dperelman3@gmail.com>
2024-09-10 14:29:26 +0300

    filter projects by divisions fix

--- src/app/panel/projectsPanel/page.jsx
+++ src/app/panel/projectsPanel/page.jsx
@@ -1,11 +1,13 @@
 import styles from "./projectsPanel.module.css";
 import CustomHead from "@/components/customHead/CustomHead";
 import ProjectsContainer from "@/components/projectsContainer/ProjectsContainer";
-import { getProjects } from "@/lib/data";
+import { getDivisions, getProjects } from "@/lib/data";
 
 const AdminProjectsPage = async () => {
-  // const data = await getProjects();
-  // const projects = JSON.parse(JSON.stringify(data));
+  const projectsData = await getProjects();
+  const projects = JSON.parse(JSON.stringify(projectsData));
+  const divisionsData = await getDivisions();
+  const divisions = JSON.parse(JSON.stringify(divisionsData));
 
   return (
     <div className={styles.container}>
@@ -15,7 +17,12 @@ const AdminProjectsPage = async () => {
       />
       <h1 className={styles.pageHeader}>עריכת פרויקטים</h1>
       <div className={styles.pageHeader}></div>
-      {/* <ProjectsContainer projects={projects} link="/panel/projectsPanel/" /> */}
+
+      <ProjectsContainer
+        projects={projects}
+        divisions={divisions}
+        link="/panel/projectsPanel/"
+      />
     </div>
   );
 };
