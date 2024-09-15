7e8a30bfaa2fa43617753f11565e0d902c10e77b
David Perelman <dperelman3@gmail.com>
2024-09-15 16:15:04 +0300

    change home page video

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -3,7 +3,7 @@ import BackgroundVideo from "next-video/background-video";
 const Video = () => {
   return (
     <main>
-      <BackgroundVideo src="https://res.cloudinary.com/dyzl8mvwt/video/upload/v1724840260/er8y0ugumauuaexgcfii.mp4"></BackgroundVideo>
+      <BackgroundVideo src="https://res.cloudinary.com/dflevhwgh/video/upload/v1726405989/t7v0fqqwi2dokldckuzi.mov"></BackgroundVideo>
     </main>
   );
 };
