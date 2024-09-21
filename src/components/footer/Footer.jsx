add75224bd91f8a64691c7b27fb8376799544e64
David Perelman <dperelman3@gmail.com>
2024-09-22 00:48:19 +0300

    footer styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,33 +1,35 @@
 import Link from "next/link";
 import styles from "./footer.module.css";
+import { FaCopyright } from "react-icons/fa";
+import { FaRegCopyright } from "react-icons/fa6";
 
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
     <div className={styles.footer}>
-      <div className={styles.footerRight}>
-        <span>&copy;</span>
-        <span>יאיר פרלמן</span>
-        <div>
-          <span className={styles.span}></span>
-        </div>
-        <span>{year}</span>
-        <div>
-          <span className={styles.span}></span>
-        </div>
-        <span>
-          <Link href="/privacy-policy">תנאי שימוש ומדיניות פרטיות</Link>
-        </span>
+      <span className={styles.copyright}>
+        <FaRegCopyright />
+      </span>
+      <span>יאיר פרלמן</span>
+      <div>
+        <span className={styles.span}></span>
       </div>
-      {/* <span>-</span> */}
-      <div className={styles.footerLeft}>
-        <span>
-          עיצוב ופיתוח האתר <span>:</span>
-          <Link target="_blank" href="https://wa.me/972584455456">
-            &nbsp;דוד פרלמן
-          </Link>
-        </span>
+      <span>{year}</span>
+      <div>
+        <span className={styles.span}></span>
       </div>
+      <span>
+        עיצוב ופיתוח אתר <span>-</span>
+        <Link target="_blank" href="https://wa.me/972584455456">
+          &nbsp;דוד פרלמן
+        </Link>
+      </span>
+      <div>
+        <span className={styles.span}></span>
+      </div>
+      <span>
+        <Link href="/privacy-policy">תנאי שימוש ומדיניות פרטיות</Link>
+      </span>
     </div>
   );
 };
