8b0a7ddb9f942312c3e54b924883609f455cce08
David Perelman <dperelman3@gmail.com>
2024-09-22 12:46:33 +0300

    more check

--- src/app/panel/projectsPanel/page.jsx
+++ src/app/panel/projectsPanel/page.jsx
@@ -1,17 +1,48 @@
+"use client";
+
 import styles from "./projectsPanel.module.css";
 import CustomHead from "@/components/customHead/CustomHead";
 import ProjectsContainer from "@/components/projectsContainer/ProjectsContainer";
+import { useEffect, useState } from "react";
 
-const AdminProjectsPage = async () => {
-  const resProjects = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/projects`
-  );
-  const projects = await resProjects.json();
+const AdminProjectsPage = () => {
+  const [projects, setProjects] = useState([]);
+  const [divisions, setDivisions] = useState([]);
 
-  const resDivisions = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/divisions`
-  );
-  const divisions = await resDivisions.json();
+  useEffect(() => {
+    const getProjects = async () => {
+      try {
+        const resProjects = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/projects`
+        );
+        if (!resProjects.ok)
+          throw new Error(`HTTP error! status: ${resProjects.status}`);
+
+        const data = await resProjects.json();
+        if (data.projects) setProjects(data.projects);
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+      }
+    };
+
+    const getDivisions = async () => {
+      try {
+        const resDivisions = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/divisions`
+        );
+        if (!resDivisions.ok)
+          throw new Error(`HTTP error! status: ${resDivisions.status}`);
+
+        const data = await resDivisions.json();
+        if (data.divisions) setDivisions(data.divisions);
+      } catch (error) {
+        console.error("Error fetching divisions:", error);
+      }
+    };
+
+    getProjects();
+    getDivisions();
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -22,11 +53,21 @@ const AdminProjectsPage = async () => {
       <h1 className={styles.pageHeader}>עריכת פרויקטים</h1>
       <div className={styles.pageHeader}></div>
 
-      <ProjectsContainer
+      {projects.length > 0 && divisions.length > 0 ? (
+        <ProjectsContainer
+          projects={projects}
+          divisions={divisions}
+          link="/panel/projectsPanel/"
+        />
+      ) : (
+        <></>
+      )}
+
+      {/* <ProjectsContainer
         projects={projects.projects}
         divisions={divisions.divisions}
         link="/panel/projectsPanel/"
-      />
+      /> */}
     </div>
   );
 };
